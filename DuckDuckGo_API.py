8aec7664aeb25002ab133359eb10a363cc7a1336
pvscott87 <pvscott@my.waketech.edu>
2022-11-07 16:49:57 -0500

    Removed main

--- DuckDuckGo_API.py
+++ DuckDuckGo_API.py
@@ -6,17 +6,8 @@ def ddg_query():
     url_ddg = "https://api.duckduckgo.com"
     resp = requests.get(url_ddg + "/?q=presidents+of+the+united+states&format=json")
     rsp_data = resp.json()
-    for data in  rsp_data["RelatedTopics"]:
+    for data in rsp_data["RelatedTopics"]:
         info = data["Text"]
-        name = info.split(" -")[0]
-        ##president = name.split(" ")[-1]
-        presidents_list.append(name)
+        president = info.split(" -")[0]
+        presidents_list.append(president)
     return presidents_list
-
-def main():
-    list = ddg_query()
-    print(list)
-
-
-if __name__ == '__main__':
-    main()
