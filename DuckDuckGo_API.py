4f3ebd932bb7af4ade50d97ca45a70738a05cc2d
pvscott87 <pvscott@my.waketech.edu>
2022-11-07 14:50:50 -0500

    Added DuckDuckGo query

--- DuckDuckGo_API.py
+++ DuckDuckGo_API.py
@@ -1,13 +1,21 @@
 import requests
 
-url_ddg = "https://api.duckduckgo.com"
-resp = requests.get(url_ddg + "/?q=DuckDuckGo&format=json")
-rsp_data = resp.json()
-print(rsp_data["Heading"])
-'''
-url_list = []
-for photo in json_data:
-    url_list.append(photo['url'])
-print(len(url_list))
-print(len(set(url_list)))
-'''
+
+def ddg_query():
+    presidents_list = []
+    url_ddg = "https://api.duckduckgo.com"
+    resp = requests.get(url_ddg + "/?q=presidents+of+the+united+states&format=json")
+    rsp_data = resp.json()
+    for data in  rsp_data["RelatedTopics"]:
+        president = data["Text"]
+
+        presidents_list.append(president)
+    return presidents_list
+
+def main():
+    list = ddg_query()
+    print(list)
+
+
+if __name__ == '__main__':
+    main()
